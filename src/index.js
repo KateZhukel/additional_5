13c32ed8e4d2aca9580ea107688c01a122711f62
Kate Zhukel <zhukelkatya@gmail.com>
2018-09-16 23:38:29 +0300

    resolved function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let openBrackets = bracketsConfig.map(a => a[0]);
+    let closeBrackets = bracketsConfig.map(a => a[1]);
+    for (let i = 0; i < str.length; i++) {
+        if(openBrackets.includes(str[i])){
+            if(stack.length > 0 && stack.slice(-1)[0] === str[i] && closeBrackets.includes(str[i])
+                    && openBrackets.includes(str[i])){
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        } else {
+            if(stack.length === 0 || !checkPair(stack.pop(),str[i], bracketsConfig)) {
+                return false;
+            }
+        }
+    }
+    if(stack.length === 0 ){
+        return true;
+    } else {
+        return false;
+    }
+};
+function checkPair(bracketOpen, bracketClose, bracketsArray) {
+    for (let i = 0; i < bracketsArray.length; i++) {
+        if(bracketsArray[i][0] === bracketOpen && bracketsArray[i][1] === bracketClose){
+            return true;
+        }
+    }
+    return false;
 }
